cad48ddd74d58fac9a10c336bb4497a121557392
Franck Tameze <francktameze5@gmail.com>
2022-02-24 00:25:02 +0100

    Create EuclideanDistanceCalculator.java

---
+++ src/main/java/projekt/base/EuclideanDistanceCalculator.java
@@ -0,0 +1,15 @@
+package projekt.base;
+
+public class EuclideanDistanceCalculator implements DistanceCalculator{
+    /**
+     * calculate the distance between two locations
+     * @param location1 a location gives
+     * @param location2 a location gives
+     * @return the distance between two locations
+     */
+    @Override
+    public double calculateDistance(Location location1, Location location2) {
+        return Math.sqrt((Math.pow(location1.getX() - location2.getX(), 2) +
+            Math.pow(location1.getY() - location2.getY(), 2)));
+    }
+}

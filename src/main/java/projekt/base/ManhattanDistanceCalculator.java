20963d2f151a682c925edaca6068714e66a0b0ad
Franck Tameze <francktameze5@gmail.com>
2022-02-24 00:31:43 +0100

    Create ManhattanDistanceCalculator.java

---
+++ src/main/java/projekt/base/ManhattanDistanceCalculator.java
@@ -0,0 +1,14 @@
+package projekt.base;
+
+public class ManhattanDistanceCalculator implements DistanceCalculator{
+    /**
+     * calculate the manhattan distance between two locations
+     * @param location1 a location gives
+     * @param location2 a location gives
+     * @return the manhattan distance between two locations
+     */
+    @Override
+    public double calculateDistance(Location location1, Location location2) {
+        return Math.abs(location1.getX() - location2.getX()) + Math.abs(location1.getY() - location2.getY());
+    }
+}

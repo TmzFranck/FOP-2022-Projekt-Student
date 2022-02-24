d24a140862bf6064bb6c8aa59c8d8b19edca2a11
Franck Tameze <francktameze5@gmail.com>
2022-02-24 01:17:21 +0100

    Create TimeInterval.java

---
+++ src/main/java/projekt/base/TimeInterval.java
@@ -0,0 +1,47 @@
+package projekt.base;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class TimeInterval {
+    private final LocalDateTime start;
+    private final LocalDateTime end;
+
+    /**
+     * a constructor
+     * @param start the start time
+     * @param end the end time
+     * @throws IllegalAccessException when start after end is
+     */
+    public TimeInterval(LocalDateTime start, LocalDateTime end) throws IllegalAccessException {
+        if (start == null) throw new NullPointerException("start");
+        else if(end == null) throw new NullPointerException("end");
+        if (start.isAfter(end)) throw new IllegalAccessException(String.format("Start %s is after end %s", start, end));
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     *
+     * @return start time
+     */
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    /**
+     *
+     * @return end time
+     */
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    /**
+     *
+     * @return a duration time between two start and end
+     */
+    public Duration getDuration(){
+        return Duration.between(getStart(), getEnd());
+    }
+}

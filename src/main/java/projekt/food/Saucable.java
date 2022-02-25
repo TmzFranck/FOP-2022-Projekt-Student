591adb9dd7bad045ad6ae66350c96473c0db7da4
Franck Tameze <francktameze5@gmail.com>
2022-02-25 23:40:03 +0100

    Create Saucable.java

---
+++ src/main/java/projekt/food/Saucable.java
@@ -0,0 +1,12 @@
+package projekt.food;
+
+/**
+ *
+ */
+public interface Saucable extends Food {
+    /**
+     *
+     * @return  a string identifying the sauce
+     */
+     String getSauce();
+}

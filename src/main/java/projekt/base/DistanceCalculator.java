fc72ed5fe93b3d913f97fccf2661f52a96b02aea
Franck Tameze <francktameze5@gmail.com>
2022-02-25 23:39:38 +0100

    Update DistanceCalculator.java

--- src/main/java/projekt/base/DistanceCalculator.java
+++ src/main/java/projekt/base/DistanceCalculator.java
@@ -1,11 +1,14 @@
 package projekt.base;
 
+/**
+ * the distance from the pizzeria to the customer.
+ */
 public interface DistanceCalculator {
     /**
-     * calculate the distance between two locations
+     * calculate the distance between two locations.
      * @param location1 a location gives
      * @param location2 a location gives
      * @return the distance between two locations
      */
-    public double calculateDistance(Location location1, Location location2);
+    double calculateDistance(Location location1, Location location2);
 }

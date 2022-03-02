2504182e1c6f742eea177e4b3abbcadf67564e77
buobuo-buo <wuwujiawei@gmail.com>
2022-03-02 08:13:17 +0100

    H2.2 Add interfaces with mutator functionality

--- src/main/java/projekt/food/Pasta.java
+++ src/main/java/projekt/food/Pasta.java
@@ -1,5 +1,7 @@
 package projekt.food;
 
+import java.util.function.DoubleUnaryOperator;
+
 /**
  *
  */
@@ -9,4 +11,23 @@ public interface Pasta extends Saucable {
      * @return the thickness of the pasta
      */
     double getThickness();
+
+    /**
+     * TODO: add description
+     */
+    interface Config extends Saucable.Config {
+        /**
+         * chain the current thickness with given thicknessMutator
+         * and save it internally
+         * @param thicknessMutator the given thicknessMutator
+         */
+        void thickness (DoubleUnaryOperator thicknessMutator);
+
+        /**
+         * getter method returns the internally saved
+         * thicknessMutator
+         * @return internally saved thicknessMutator
+         */
+        DoubleUnaryOperator getThicknessMutator();
+    }
 }
--- src/main/java/projekt/food/Saucable.java
+++ src/main/java/projekt/food/Saucable.java
@@ -1,5 +1,7 @@
 package projekt.food;
 
+import java.util.function.UnaryOperator;
+
 /**
  *
  */
@@ -9,4 +11,23 @@ public interface Saucable extends Food {
      * @return  a string identifying the sauce
      */
      String getSauce();
+
+    /**
+     * TODO: add description
+     */
+     interface Config extends Food.Config {
+        /**
+         * chain the current sauce with given sauceMutator
+         * and save it internally
+         * @param sauceMutator the given sauceMutator
+         */
+         void sauce (UnaryOperator<String> sauceMutator);
+
+        /**
+         * getter method returns the internally saved
+         * sauceMutator
+         * @return the internally saved sauceMutator
+         */
+         UnaryOperator<String> getSauceMutator();
+     }
 }

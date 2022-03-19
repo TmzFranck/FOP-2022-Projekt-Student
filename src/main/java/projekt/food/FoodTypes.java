d46384a5f07e9df27b1b16652a3c9df3c2c660c7
Franck Tameze <francktameze5@gmail.com>
2022-03-19 13:36:37 +0100

    Update FoodTypes.java

--- src/main/java/projekt/food/FoodTypes.java
+++ src/main/java/projekt/food/FoodTypes.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * food types.
+ */
 public final class FoodTypes {
     final static FoodType PIZZA = new FoodTypeImpl<Pizza, Pizza.Config>(
         "Pizza",
